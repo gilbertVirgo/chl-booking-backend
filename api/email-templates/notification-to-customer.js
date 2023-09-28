f3a5b14958c78ce898def62a8aa043c18a798aa8
Gil <gilbertjvirgo@gmail.com>
2023-09-28 19:03:51 +0100

    Fixed extravagant line breaks on email template

--- api/email-templates/notification-to-customer.js
+++ api/email-templates/notification-to-customer.js
@@ -1,14 +1,14 @@
-export default ({ firstname, group_size }) => `Dear ${firstname},\n\n
+export default ({ firstname, group_size }) => `Dear ${firstname},
 
-Thank you for getting in touch.\n\n
+Thank you for getting in touch.
 
 ${
 	+group_size >= 4
 		? `We will aim to confirm your booking as soon as possible.`
 		: `Our minimum group size is four people, so we will make provisional bookings for the date(s) that you have requested and will confirm when others approach us.`
-}\n\n
+}
 
-Christian Heritage London\n\n
+Christian Heritage London
 
-___\n
+___
 Remember your leaders, who spoke the word of God to you. Consider the outcome of their way of life and imitate their faith. (Hebrews 13:7)`;
